2c362b70ad6fc14df053c7114641c11898406ded
ShabnaH131 <shabnah5252@gmail.com>
2024-11-05 12:31:23 +0530

    Maven Commit

---
+++ Newmavenproject/src/main/java/maven/MavenProject.java
@@ -0,0 +1,17 @@
+package maven;
+
+public class MavenProject {
+
+	public static void main(String[] args) {
+		int b=10;
+		String v=new String("Welcome");
+		StringBuffer a=new StringBuffer("Welcome");
+		System.out.println(a.reverse());
+		System.out.println(a.length());
+		System.out.println(String.valueOf(b));
+		System.out.println(v.equals(a));
+		System.out.println(a.append("Java"));
+
+	}
+
+}
